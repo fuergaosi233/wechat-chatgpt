fdde79f5f04f5e369aca64ac0d8aa7a519181e5b
敬文 | 罗千洪 <jinwen.lqh@dtyunxi.com>
2023-02-09 09:52:32 +0800

    fix: 去掉多余的password配置相关信息

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,6 @@ if (fs.existsSync("./config.yaml")) {
     chatGPTAccountPool: [
       {
         email: process.env.CHAT_GPT_EMAIL,
-        password: process.env.CHAT_GPT_PASSWORD,
         apiKey: process.env.CHAT_GPT_API_KEY
       },
     ],
