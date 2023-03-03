c7e244a7a319e3684cc1f8d48d4e62959ad95924
RealTong <i@realtong.cn>
2023-03-03 11:23:57 +0800

    fix: 🐛 Resolve git conflicts
    Resolve conflicts caused by git

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -1,6 +1,5 @@
 import {config} from "./config.js";
 
-<<<<<<< HEAD
 let apiKey = config.openai_api_key;
 let model = config.model;
 const sendMessage = async (message: string) => {
@@ -25,46 +24,10 @@ const sendMessage = async (message: string) => {
     // console.log("response", response.json().then((data) => data.response))
     return response.json()
       .then((data) => data.choices[0].message.content);
-=======
-let url = config.url
-config.url = "http://localhost:4000";
-const oneTimeMessage = async (message: string) => {
-  try {
-    const response = await fetch(`${url}/message`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        message
-      })
-    });
-    return response.json().then((data) => data.response);
->>>>>>> origin/chatgpt-api
   } catch (e) {
     console.error(e)
     return "Something went wrong"
   }
 }
-<<<<<<< HEAD
+
 export {sendMessage};
-=======
-const sessionMessage = async (message: string, sessionId: string) => {
-  try {
-    const response = await fetch(`${url}/message/${sessionId}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        message
-      })
-    });
-    return response.json().then((data) => data.response);
-  } catch (e) {
-    console.error(e)
-    return "Something went wrong"
-  }
-}
-export {oneTimeMessage, sessionMessage};
->>>>>>> origin/chatgpt-api
